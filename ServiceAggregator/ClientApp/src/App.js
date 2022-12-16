1a724a74eb1d5453d2138dd15928260977fb541f
Kirill Yablonsky <blackshark564@gmail.com>
2022-12-16 23:24:52 +0300

    bruh, cringe setup

--- ServiceAggregator/ClientApp/src/App.js
+++ ServiceAggregator/ClientApp/src/App.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import React, { Component, useEffect } from "react";
+import axios from "axios";
+import { Route } from "react-router";
+import { Layout } from "./components/Layout";
+import { Home } from "./components/Home";
+import { FetchData } from "./components/FetchData";
+import { Counter } from "./components/Counter";
 
-import './custom.css'
+import "./custom.css";
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+export const App = () => {
+    useEffect(() => {
+        axios("api/WorkSections/GetListOfSections");
+    }, []);
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
+        <Layout>
+            <Route exact path="/" component={Home} />
+            <Route path="/counter" component={Counter} />
+            <Route path="/fetch-data" component={FetchData} />
+        </Layout>
     );
-  }
-}
+};

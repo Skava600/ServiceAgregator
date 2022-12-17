cec079ddc1c483ff0791c68404364ebc3b2c3e02
Skava600 <56450477+Skava600@users.noreply.github.com>
2022-12-17 21:38:12 +0300

    cool fix.

--- ServiceAggregator/Models/RegistrationModel.cs
+++ ServiceAggregator/Models/RegistrationModel.cs
@@ -6,7 +6,7 @@
         public string LastName { get; set; } = "";
         public string Patronym { get; set; } = "";
         public string Email { get; set; } = "";
-        public string PhoneNumber { get; set; } =""
+        public string PhoneNumber { get; set; } = "";
         public string? Location { get; set; }
         public string Password { get; set; } = "";
         public string PasswordConfirm { get; set; } = "";

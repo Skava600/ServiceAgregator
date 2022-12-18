bc4a178bc630cd068cf6df0f8386bfe22b5de8cf
Skava600 <56450477+Skava600@users.noreply.github.com>
2022-12-19 00:16:17 +0300

    Fix marking order done in orders api.

--- ServiceAggregator/Models/OrderResult.cs
+++ ServiceAggregator/Models/OrderResult.cs
@@ -12,5 +12,6 @@
         public const string ERROR_WRONG_CANCELING_OPERATION = "Нельзя отменить выполненный или отменный заказ.";
         public const string ERROR_WRONG_MARKING_DONE_OPERATION = "Нельзя пометить сделанным заказ не в исполнении.";
         public const string ERROR_UPDATING_NOT_OPEN_ORDER = "Нельзя обновить не открытый заказ.";
+        public const string ERROR_NO_CUSTOMER_MAKING_ORDER = "За вашим заказом не закреплен исполнитель.";
     }
 }
